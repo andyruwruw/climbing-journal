16b56a65ecd2d648aa48edd08c015ab60f9c7586
Andrew Young <andrew@youngshome.com>
2023-02-06 07:58:55 -0800

    New Client

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,25 +1,22 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-
-import HomeView from '../views/home/index.vue';
+import HomeView from '../views/HomeView.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: HomeView,
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/login/index.vue'),
-  },
-  {
-    path: '/profile/:id',
-    name: 'Profile',
-    component: () => import('../views/profile/index.vue'),
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
   },
 ];
 
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,12 +1,7 @@
-module.exports = {
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
   transpileDependencies: [
     'vuetify',
   ],
-  devServer: {
-    proxy: {
-      '^/api': {
-        target: 'http://localhost:3000',
-      },
-    },
-  },
-};
+});
